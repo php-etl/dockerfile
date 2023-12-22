aff2e4ffbe94891afd79682b144bf8129f3fd91c
sebprt <sebastien@kiboko.fr>
2023-12-22 16:45:44 +0100

    Added a new command to configure composer config

---
+++ src/PHP/ComposerConfig.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kiboko\Component\Dockerfile\PHP;
+
+use Kiboko\Component\Dockerfile\Dockerfile;
+
+final readonly class ComposerConfig implements Dockerfile\LayerInterface, \Stringable
+{
+    public function __construct(
+        private string $host,
+        private string $token,
+    )
+    {
+    }
+
+    public function __toString(): string
+    {
+        return (string) new Dockerfile\Run(sprintf(
+            <<<'RUN'
+                composer config --global %s %s
+                RUN,
+            $this->host,
+            $this->token,
+        ));
+    }
+}
