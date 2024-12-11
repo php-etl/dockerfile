0e5ef84adb5b32cb944f8dd18d8f4783c5eb2aac
Grégory Planchat <gregory@gyroscops.com>
2024-12-11 11:19:36 +0100

    feat: added aliases to the FROM command

--- src/Dockerfile/From.php
+++ src/Dockerfile/From.php
@@ -6,12 +6,18 @@ namespace Kiboko\Component\Dockerfile\Dockerfile;
 
 final readonly class From implements LayerInterface, \Stringable
 {
-    public function __construct(private string $source)
-    {
+    public function __construct(
+        private string $source,
+        private ?string $alias = null,
+    ) {
     }
 
     public function __toString(): string
     {
-        return sprintf('FROM %s', $this->source);
+        if ($this->alias === null) {
+            return sprintf('FROM %s', $this->source);
+        }
+
+        return sprintf('FROM %s AS %s', $this->source, $this->alias);
     }
 }
